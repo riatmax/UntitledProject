3d6f711ddff4780d499793cb7354b2b33bec0da2
riatmax <riatmax@gmail.com>
2024-11-11 15:27:28 -0500

    kept testing progen

--- Assets/Scripts/ProGenScripts/ProGen.cs
+++ Assets/Scripts/ProGenScripts/ProGen.cs
@@ -13,31 +13,86 @@ public class ProGen : MonoBehaviour
     [Header("Generation Prefabs")]
     public GameObject blockGameObj;
     public GameObject objToSpawn;
+    public GameObject player;
 
     [Header("Information")]
     public List<Vector3> blockPositions = new List<Vector3>();
 
+    private Vector3 startPos;
+    private Hashtable blockContainer = new Hashtable();
+
     private void Start()
     {
-        for (int x = 0; x < worldSizeX; x++)
+        for (int x = -worldSizeX; x < worldSizeX; x++)
         {
-            for (int z = 0; z < worldSizeZ; z++)
+            for (int z = -worldSizeZ; z < worldSizeZ; z++)
             {
-                Vector3 pos = new Vector3(x * gridOffset, 
+                Vector3 pos = new Vector3(x * 1 + startPos.x, 
                     generateNoise(x, z, 8f) * noiseHeight, 
-                    z * gridOffset);
+                    z * 1 + startPos.z);
 
                 GameObject block = Instantiate(blockGameObj, pos, Quaternion.identity);
 
+                blockContainer.Add(pos, block);
                 blockPositions.Add(block.transform.position);
-
                 block.transform.SetParent(this.transform);
             }
         }
-        SpawnObject();
+        //SpawnObject();
     }
+    private void Update()
+    {
+        if (Mathf.Abs(xPlayerMove) >= 1 || Mathf.Abs(zPlayerMove) >= 1)
+        {
+            for (int x = -worldSizeX; x < worldSizeX; x++)
+            {
+                for (int z = -worldSizeZ; z < worldSizeZ; z++)
+                {
+                    Vector3 pos = new Vector3(x * 1 + xPlayerLocation,
+                        generateNoise(x + xPlayerLocation, z + zPlayerLocation, 8f) * noiseHeight,
+                        z * 1 + zPlayerLocation);
 
-    private void SpawnObject()
+                    if (!blockContainer.ContainsKey(pos))
+                    {
+                        GameObject block = Instantiate(blockGameObj, pos, Quaternion.identity);
+
+                        blockContainer.Add(pos, block);
+                        blockPositions.Add(block.transform.position);
+                        block.transform.SetParent(this.transform);
+                    }
+                }
+            }
+        }
+    }
+    private int xPlayerMove
+    {
+        get
+        {
+            return (int)(player.transform.position.x - startPos.x);
+        }
+    }
+    private int zPlayerMove
+    {
+        get
+        {
+            return (int)(player.transform.position.z - startPos.z);
+        }
+    }
+    private int xPlayerLocation
+    {
+        get
+        {
+            return (int)Mathf.Floor(player.transform.position.x);
+        }
+    }
+    private int zPlayerLocation
+    {
+        get
+        {
+            return (int)Mathf.Floor(player.transform.position.z);
+        }
+    }
+   /* private void SpawnObject()
     {
         for (int c = 0; c < 20; c++)
         {
@@ -57,7 +112,7 @@ public class ProGen : MonoBehaviour
 
         blockPositions.RemoveAt(randIndex);
         return newPos;
-    }
+    }*/
 
     private float generateNoise(int x, int z, float detailScale)
     {
