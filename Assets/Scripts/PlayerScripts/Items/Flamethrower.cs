c46b7ed4d09f07a9ab3aef7516928a3af923e829
riatmax <riatmax@gmail.com>
2024-11-15 13:01:56 -0500

    did a lil work

--- Assets/Scripts/PlayerScripts/Items/Flamethrower.cs
+++ Assets/Scripts/PlayerScripts/Items/Flamethrower.cs
@@ -2,8 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flamethrower : MonoBehaviour
+public class Flamethrower : EquippableItem
 {
+    public override void LeftClick()
+    {
+        if (Input.GetAxisRaw("Fire") > 0)
+        {
+
+        }
+    }
+
+    public override void RightClick()
+    {
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
